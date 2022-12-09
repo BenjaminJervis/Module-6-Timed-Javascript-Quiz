7acd856a361a75c937542184026439599a568eec
unknown <ben.jervis.keele@googlemail.com>
2022-12-09 18:48:45 +0000

    folder structure updated

---
+++ quiz/assets/js/questions.js
@@ -0,0 +1,96 @@
+/** 
+ * Criteria 
+ */
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+
+
+ // Define a set of questions - Here I have created a set of questions that I want the user to answer 
+ var questions = [{
+
+
+        question: "What is Javascript?",
+        choices:   ["a. The building blocks of a website", "b. A programming language", "c. A place where users enter information on a website", "d. A type of font"],
+        answer: "b. A programming language"
+    },
+
+    {
+        question: "which file extension is used to save a Javascript file?",
+        choices: ["a. .pdf", "b. .HTML ", "c. .css", "d. .js"],
+        answer: "d. .js"
+    },
+
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+        answer: "d. <script>"
+
+    },
+
+    {
+        question: "How do you add comments in a JavaScript?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "a. //This is a comment"
+    },
+
+    {
+        question: "Why do we use comments in a JavaScript?",
+        choices: ["a. To explain the code", "b. To call a function", "c. To store variables", "d. To make the website interactive"],
+        answer: "a. To explain the code"
+
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },
+
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+
+    {
+        question: "How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b. function myFunction()"
+    },
+
+    {
+        question: "How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c. myFunctions()"
+    },
+
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
+
+    {
+        question: "The first index of an array is always____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+
+    {
+        question: "Who invented Javascript?",
+        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
+        answer: "c. Brendan Eich"
+    },
+    
+];
